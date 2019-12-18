16be8cc6ad299648b638e4ba67e39560b26cda1d
Brandon R-G <37788621+craftlinker@users.noreply.github.com>
2019-12-18 14:34:40 -0500

    Add files via upload

---
+++ jqueryuploader.js
@@ -0,0 +1,36 @@
+// // https://bootsnipp.com/snippets/eNbOa#comments 
+// fileUploder 
+// "Upload Image w Preview & Filename"
+// Bootstrap 3.3.0 Snippet by suketran
+
+
+
+
+
+    $('.btn-file :file').on('fileselect', function(event, label) {
+        
+        var input = $(this).parents('.input-group').find(':text'),
+            log = label;
+        
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+    
+    });
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#img-upload').attr('src', e.target.result);
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#imgInp").change(function(){
+        readURL(this);
+    }); 	
+            
